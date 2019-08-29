5155bc82e0e483c25e20781ea36a600bde46684e
Jed Laundry <jlaundry@jlaundry.com>
2019-08-29 14:10:53 +1200

    Initial project

---
+++ TypeClipboard/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TypeClipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Type";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(317, 44);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Type Clipboard";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Enter += new System.EventHandler(this.Form1_Enter);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TypeClipboard/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypeClipboard
+{
+    public partial class Form1 : Form
+    {
+        const int WS_EX_NOACTIVATE = 0x08000000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Thread.Sleep(100);
+        }
+
+        //
+        // https://stackoverflow.com/questions/2795558/c-sharp-sending-keyboard-events-to-last-selected-window
+        // Chris Taylor, licenced under CC BY-SA 3.0
+        //
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.ExStyle |= WS_EX_NOACTIVATE;
+                return param;
+            }
+        }
+
+        public void TypeClipboard()
+        {
+            if (Clipboard.ContainsText(TextDataFormat.UnicodeText))
+            {
+                String clipboard = Clipboard.GetText(TextDataFormat.UnicodeText);
+                Thread.Sleep(2000);
+                foreach (Char c in clipboard.ToCharArray())
+                {
+                    // Some characters have special meaning
+                    // https://docs.microsoft.com/en-us/office/vba/language/reference/user-interface-help/sendkeys-statement
+                    switch (c)
+                    {
+                        case '\n':
+                            return;
+                        case '\r':
+                            return;
+                        case '{':
+                            SendKeys.Send("{{}");
+                            break;
+                        case '}':
+                            SendKeys.Send("{}}");
+                            break;
+                        case '+':
+                            SendKeys.Send("{+}");
+                            break;
+                        case '^':
+                            SendKeys.Send("{^}");
+                            break;
+                        case '%':
+                            SendKeys.Send("{%}");
+                            break;
+                        case '~':
+                            SendKeys.Send("{~}");
+                            break;
+                        case '(':
+                            SendKeys.Send("{(}");
+                            break;
+                        case ')':
+                            SendKeys.Send("{)}");
+                            break;
+                        default:
+                            SendKeys.Send(c.ToString());
+                            break;
+                    }
+                    Thread.Sleep(10);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TypeClipboard();
+        }
+
+        public void UpdateTextbox()
+        {
+            if (Clipboard.ContainsText(TextDataFormat.UnicodeText))
+            {
+                String clipboard = Clipboard.GetText(TextDataFormat.UnicodeText);
+                StringBuilder sb = new StringBuilder();
+                sb.Append(clipboard.Substring(0, Math.Min(3, clipboard.Length)));
+                sb.Append(" ... ");
+                sb.Append(clipboard.Substring(clipboard.Length - 3));
+                textBox1.Text = sb.ToString();
+            }
+            else
+            {
+                textBox1.Text = "No UnicodeText in clipboard";
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            UpdateTextbox();
+        }
+
+        private void Form1_Enter(object sender, EventArgs e)
+        {
+            UpdateTextbox();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            UpdateTextbox();
+        }
+    }
+}
