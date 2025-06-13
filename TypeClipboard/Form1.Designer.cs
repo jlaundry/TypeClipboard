911d06154ca3e4fbfc114be4e7d0c9909959aa25
Jed Laundry <jlaundry@jlaundry.com>
2025-06-14 09:07:47 +1200

    restore SendKeys as an option

--- TypeClipboard/Form1.Designer.cs
+++ TypeClipboard/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace TypeClipboard
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TypeClipboard
 {
     partial class Form1
     {
@@ -23,14 +26,12 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.chkHotkey = new System.Windows.Forms.CheckBox();
@@ -39,6 +40,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.chkEnter = new System.Windows.Forms.CheckBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // textBox1
@@ -63,7 +66,7 @@
             // chkHotkey
             // 
             this.chkHotkey.AutoSize = true;
-            this.chkHotkey.Location = new System.Drawing.Point(172, 42);
+            this.chkHotkey.Location = new System.Drawing.Point(102, 98);
             this.chkHotkey.Name = "chkHotkey";
             this.chkHotkey.Size = new System.Drawing.Size(73, 17);
             this.chkHotkey.TabIndex = 2;
@@ -104,7 +107,7 @@
             // chkEnter
             // 
             this.chkEnter.AutoSize = true;
-            this.chkEnter.Location = new System.Drawing.Point(244, 42);
+            this.chkEnter.Location = new System.Drawing.Point(177, 98);
             this.chkEnter.Name = "chkEnter";
             this.chkEnter.Size = new System.Drawing.Size(78, 17);
             this.chkEnter.TabIndex = 7;
@@ -119,10 +122,39 @@
             // 
             this.toolTip1.ShowAlways = true;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SendInput",
+            "SendKeys"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 95);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(293, 98);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(29, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Help";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
             // Form1
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(334, 99);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 124);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.chkEnter);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
@@ -148,14 +180,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox chkHotkey;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.CheckBox chkEnter;
-        private System.Windows.Forms.ToolTip toolTip1;
+        private TextBox textBox1;
+        private Button button1;
+        private CheckBox chkHotkey;
+        private Button button2;
+        private TextBox textBox2;
+        private Button button3;
+        private CheckBox chkEnter;
+        private ToolTip toolTip1;
+        private ComboBox comboBox1;
+        private LinkLabel linkLabel1;
     }
 }
-
